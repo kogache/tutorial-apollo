67dcb719936de2562b91be2127239c8c010bb130
h.koga.orso <h_koga@orso.jp>
2018-05-22 18:07:12 +0900

    First commit. 1.Setting up.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './data/schema';
+
+const GRAPHQL_PORT = 4010;
+const graphQLServer = express();
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+graphQLServer.listen(GRAPHQL_PORT, () =>
+  console.log(`GrapiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`)
+);
