856c3a8291b8d489f0707c60cfac8e8b62a3adfb
h.koga.orso <h_koga@orso.jp>
2018-05-24 10:44:50 +0900

    seed部分をコメント。

--- data/connectors.js
+++ data/connectors.js
@@ -19,20 +19,20 @@ const PostModel = db.define('post', {
 AuthorModel.hasMany(PostModel);
 PostModel.belongsTo(AuthorModel);
 
-casual.seed(123);
-db.sync({ force: true }).then(() => {
-  _.times(10, () => {
-    return AuthorModel.create({
-      firstName: casual.first_name,
-      lastName: casual.last_name,
-    }).then((author) => {
-      return author.createPost({
-        title: `A post by ${author.firstName}`,
-        text: casual.sentences(3),
-      });
-    });
-  });
-});
+// casual.seed(123);
+// db.sync({ force: true }).then(() => {
+//   _.times(10, () => {
+//     return AuthorModel.create({
+//       firstName: casual.first_name,
+//       lastName: casual.last_name,
+//     }).then((author) => {
+//       return author.createPost({
+//         title: `A post by ${author.firstName}`,
+//         text: casual.sentences(3),
+//       });
+//     });
+//   });
+// });
 
 const Author = db.models.author;
 const Post = db.models.post;
